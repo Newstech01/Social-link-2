8eb99042d2105deaa22fdb73fa249b9fc2ab3c34
GisellyRock <gisellydsr@gmail.com>
2023-05-21 21:30:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+VANTA.BIRDS({
+  el: '#vanta',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: true,
+  minHeight: 600.0,
+  minWeight: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  quantify: 2.0,
+  backgroundAlpha: 0.0,
+  alignment: 77.0,
+});
+
+AOS.int();
